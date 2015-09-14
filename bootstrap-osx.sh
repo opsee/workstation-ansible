f720e7797bac887b11271ca078f0354f51f82b9f
Dan Compton <dancompton@Dans-MacBook-Pro-2.local>
2015-09-14 15:47:30 -0700

    added check for brew

--- bootstrap-osx.sh
+++ bootstrap-osx.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-# Setup homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+# Check for homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    # https://github.com/mxcl/homebrew/wiki/installation
+    echo "no brew installation found. installing brew..."
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+else
+    echo "brew found. upadating..."
+    brew update
+fi
 
+# if you have permission errors
+# run sudo chown -R $USER:admin /usr/local /Library/Caches/Homebrew
 brew install ansible
-
 ansible-playbook -i hosts playbooks/workstation-osx.yml
